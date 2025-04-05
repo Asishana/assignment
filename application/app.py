e463b186fb2af510dfc9d0df0f41b5fdc559e018
IsDevOps <issyoseghale@gmail.com>
2025-04-05 22:15:50 +0100

    commit

--- application/app.py
+++ application/app.py
@@ -6,5 +6,6 @@ app = Flask(__name__)
 @app.route("/")
 def server_time():
     return {"server_time": datetime.utcnow().strftime("%Y-%m-%d %H:%M:%S")}
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
